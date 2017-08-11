7efd00d94fc5d299467fe334a1c5ec2e9b3ba31a
nin <ninian138@gmail.com>
2017-08-12 00:54:09 +0900

    change animation

--- js/main.js
+++ js/main.js
@@ -1,18 +1,29 @@
 "use strict";
 (() => {
+  const windowSize = window.screen.height;
   window.addEventListener("load", () => {
-    const windowSize = window.screen.height;
+    const content1 = document.getElementsByClassName("slide-label-content1")[0];
     const descArea = document.getElementsByClassName("main__func-description-area__wrap");
     const slide_label = document.getElementsByClassName("slide-in-label");
     const slide_item = document.getElementsByClassName("slide-in-item");
     window.addEventListener("scroll", () => {
+      if(isInView(content1)) {
+        const items = document.getElementsByClassName("main__content-wrap__item--2");
+        onElementInView(content1);
+        setTimeout(() => { onElementInView(items[0]) }, 300);
+        setTimeout(() => { onElementInView(items[1]) }, 600);
+
+      }
       Array.prototype.forEach.call(slide_label, (l, i) => {
-        if(l.getBoundingClientRect().top < windowSize*0.7) {
-          onElementInView(slide_item[i]);
+        if(isInView(l)) {
+          onElementInView(l);
+          setTimeout(() => {
+            onElementInView(slide_item[i]);
+          }, 300);
         }
       });
       Array.prototype.forEach.call(descArea, (l) => {
-        if(l.getBoundingClientRect().top < windowSize*0.7) {
+        if(isInView(l)) {
           const delay = 300;
           onElementInView(l.getElementsByClassName("main__func-description-area__wrap__text__point")[0]);
           setTimeout(() => {
@@ -31,5 +42,8 @@
   function onElementInView(element) {
     element.style.opacity = "1";
     element.style.transform = "translateY(0)";
+  }
+  function isInView(element) {
+    return (element.getBoundingClientRect().top < windowSize*0.7)
   }
 })();
