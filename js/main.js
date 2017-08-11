452965c428f8d6ff63ebce17165ad2d21bf21e3b
nin <ninian138@gmail.com>
2017-08-11 23:09:54 +0900

    amimation v1

--- js/main.js
+++ js/main.js
@@ -1,24 +1,35 @@
 "use strict";
 (() => {
-  let dom;
-  let area;
   window.addEventListener("load", () => {
     const windowSize = window.screen.height;
-    area = document.getElementsByClassName("main__func-description-area__wrap")[0];
-    dom = document.getElementsByClassName("animation1")[0];
+    const descArea = document.getElementsByClassName("main__func-description-area__wrap");
     const slide_label = document.getElementsByClassName("slide-in-label");
     const slide_item = document.getElementsByClassName("slide-in-item");
     window.addEventListener("scroll", () => {
-      console.log(slide_label);
       Array.prototype.forEach.call(slide_label, (l, i) => {
-        if(l.getBoundingClientRect().top < windowSize/2) {
-          slide_item[i].style.opacity = 1;
-          slide_item[i].style.transform = "translateY(0)";
+        if(l.getBoundingClientRect().top < windowSize*0.7) {
+          onElementInView(slide_item[i]);
         }
       });
-      if(area.getBoundingClientRect().bottom < windowSize) {
-        dom.style.transform = "translateY(0)"
-      }
+      Array.prototype.forEach.call(descArea, (l) => {
+        if(l.getBoundingClientRect().top < windowSize*0.7) {
+          const delay = 300;
+          onElementInView(l.getElementsByClassName("main__func-description-area__wrap__text__point")[0]);
+          setTimeout(() => {
+            onElementInView(l.getElementsByClassName("main__func-description-area__wrap__text__h3")[0]);
+          }, delay);
+          setTimeout(() => {
+            onElementInView(l.getElementsByClassName("main__func-description-area__wrap__text__description")[0]);
+          }, delay * 2);
+          setTimeout(() => {
+            onElementInView(l.getElementsByClassName("main__func-description-area__wrap__img")[0])
+          }, delay * 3);
+        }
+      });
     });
   });
+  function onElementInView(element) {
+    element.style.opacity = "1";
+    element.style.transform = "translateY(0)";
+  }
 })();
