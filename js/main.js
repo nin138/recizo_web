c3c98fc07e0abf62ddea75dcd464c3773aafb114
nin <ninian138@gmail.com>
2017-08-12 14:41:26 +0900

    impl content1 animation

--- js/main.js
+++ js/main.js
@@ -1,35 +1,40 @@
-"use strict";
 (() => {
+  "use strict";
   const windowSize = window.screen.height;
+  const delay = 300;
   window.addEventListener("load", () => {
-    const content1 = document.getElementsByClassName("slide-label-content1")[0];
-    const descArea = document.getElementsByClassName("main__func-description-area__wrap");
-    const slide_label = document.getElementsByClassName("slide-in-label");
-    const slide_item = document.getElementsByClassName("slide-in-item");
+    let content1 = document.getElementsByClassName("slide-label-content1")[0];
+    let descArea = document.getElementsByClassName("main__func-description-area__wrap");
+    let slide_label = document.getElementsByClassName("slide-in-label");
+    let slide_item = document.getElementsByClassName("slide-in-item");
     window.addEventListener("scroll", () => {
       if(isInView(content1)) {
         const items = document.getElementsByClassName("main__content-wrap__item--2");
         onElementInView(content1);
-        setTimeout(() => { onElementInView(items[0]) }, 300);
-        setTimeout(() => { onElementInView(items[1]) }, 600);
+        setTimeout(() => { onElementInView(items[0]) }, delay);
+        setTimeout(() => { items[0].getElementsByClassName("main__content-wrap__item--2__hu--l")[0].classList.add("hu") }, delay*2);
+        setTimeout(() => { onElementInView(items[1]) }, delay*4);
+        setTimeout(() => { items[1].getElementsByClassName("main__content-wrap__item--2__hu--r")[0].classList.add("hu") }, delay*5);
+        content1 = null;
       }
       Array.prototype.forEach.call(slide_label, (l, i) => {
         if(isInView(l)) {
           onElementInView(l);
           setTimeout(() => {
             onElementInView(slide_item[i]);
-          }, 300);
+          }, delay);
+          l = null;
         }
       });
       Array.prototype.forEach.call(descArea, (l) => {
         if(isInView(l)) {
-          const delay = 300;
           onElementInView(l.getElementsByClassName("main__func-description-area__wrap__text__point")[0]);
           setTimeout(() => { onElementInView(l.getElementsByClassName("main__func-description-area__wrap__text__h3")[0]); }, delay);
           setTimeout(() => { onElementInView(l.getElementsByClassName("main__func-description-area__wrap__text__description")[0]); }, delay * 2);
           setTimeout(() => {
             const el = l.getElementsByClassName("main__func-description-area__wrap__img")[0];
             onElementInView(el);
+            l = null;
             setTimeout(() => { el.classList.add("animation"); }, 500);
           }, delay * 3);
         }
@@ -42,5 +47,8 @@
       element.style.transform = "translateY(0)";
     });
   }
-  function isInView(element) { return (element.getBoundingClientRect().top < windowSize*0.7)}
+  function isInView(element) {
+    if(element === null) return false;
+    return (element.getBoundingClientRect().top < windowSize*0.7)
+  }
 })();
